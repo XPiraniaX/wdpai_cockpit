e13bb05b64b9c74ea235b111de13563a9a7d95ed
Kamil Gębala <kamilgebala323@gmail.com>
2026-04-18 10:49:36 +0200

    controllers comunication setup

--- Routing.php
+++ Routing.php
@@ -1,13 +1,5 @@
 <?php
 
-require_once 'src/controllers/AppController.php';
-require_once 'src/controllers/SecurityController.php';
-require_once 'src/controllers/DashboardController.php';
-require_once 'src/controllers/CarsController.php';
-require_once 'src/controllers/MarketplaceController.php';
-require_once 'src/controllers/CommunityController.php';
-require_once 'src/controllers/SettingsController.php';
-
 class Routing {
 
     public static $routes = [
